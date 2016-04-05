5bdd8627b52018090c574066dddef1a7882f4b91
georghinkel <hinkel@fzi.de>
2016-04-05 12:45:13 +0200

    Increased version number to 1.0.4

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Resources;
 using System.Runtime.InteropServices;
 [assembly: AssemblyProduct(".NET Modelling Framework")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyFileVersion("1.0.4")]
 [assembly: NeutralResourcesLanguage("en")]
