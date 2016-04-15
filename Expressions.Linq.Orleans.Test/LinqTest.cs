3130510f61d631ae433af564e3b5ecc3f5b6f929
Benjamin Wanner <mail@benjamin-wanner.de>
2016-04-15 17:26:40 +0200

    Testing incremental where with property changed

--- Expressions.Linq.Orleans.Test/LinqTest.cs
+++ Expressions.Linq.Orleans.Test/LinqTest.cs
@@ -4,24 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NMF.Expressions.Linq.Orleans;
-using NMF.Expressions.Linq.Orleans.Interfaces;
 using Orleans;
-using Orleans.Collections;
-using Orleans.Collections.Endpoints;
 using Orleans.Collections.Observable;
 using Orleans.Collections.Utilities;
 using Orleans.Streams;
-using Orleans.Streams.Endpoints;
-using Orleans.Streams.Linq;
 using Orleans.TestingHost;
+using TestGrains;
 
 namespace Expressions.Linq.Orleans.Test
 {
     [TestClass]
     public class LinqTest : TestingSiloHost
     {
-        private IStreamProvider _provider;
         private IncrementalStreamProcessorAggregateFactory _factory;
+        private IStreamProvider _provider;
 
         [TestInitialize]
         public void TestInitialize()
@@ -47,7 +43,7 @@ namespace Expressions.Linq.Orleans.Test
 
             var resultConsumer = await collection.SelectIncremental(i => i.Item.ToString(), _factory).ToListConsumer();
 
-            var items = new List<int>() { 1, 2, 5 };
+            var items = new List<int> {1, 2, 5};
             await collection.BatchAdd(items);
 
             CollectionAssert.AreEqual(items.Select(i => i.ToString()).ToList(), resultConsumer.Items.Select(e => e.Item).ToList());
@@ -57,17 +53,32 @@ namespace Expressions.Linq.Orleans.Test
         [TestMethod]
         public async Task TestOneLevelWhereDataFilter()
         {
-            var collection = GrainClient.GrainFactory.GetGrain<IObservableContainerGrain<int>>(Guid.NewGuid());
+            var collection = GrainClient.GrainFactory.GetGrain<IObservableContainerGrain<TestObjectWithPropertyChange>>(Guid.NewGuid());
 
-            var resultConsumer = await collection.WhereIncremental(i => i.Item >= 42, _factory).ToListConsumer();
+            var resultConsumer = await collection.WhereIncremental(i => i.Item.Value >= 42, _factory).ToListConsumer();
 
-            var itemsSatisfyingCriteria = new List<int> { 42, 232130, 123, 58 };
-            var itemsViolatingCriteria = new List<int> {1, -500, 23 };
+            var itemsSatisfyingCriteria = new List<int> {42, 232130, 123, 58};
+            var itemsViolatingCriteria = new List<int> {1, -500, 23};
 
-            await collection.BatchAdd(itemsSatisfyingCriteria.Concat(itemsViolatingCriteria).ToList());
+            var hostedItems =
+                await
+                    collection.BatchAddReturnDictionary(
+                        itemsSatisfyingCriteria.Concat(itemsViolatingCriteria).Select(x => new TestObjectWithPropertyChange(x)).ToList());
 
-            CollectionAssert.AreEqual(itemsSatisfyingCriteria, resultConsumer.Items.Select(e => e.Item).ToList());
+            CollectionAssert.AreEqual(itemsSatisfyingCriteria, resultConsumer.Items.Select(e => e.Item.Value).ToList());
+
+            var itemSatisfyingCrit = hostedItems.First(i => i.Value.Value >= 42);
+            var itemViolatingCrit = hostedItems.First(i => i.Value.Value < 42);
+
+            // Make satisfying item violate
+            await collection.ExecuteSync(i => i.Value = 2, itemSatisfyingCrit.Key);
+            itemsSatisfyingCriteria.Remove(itemSatisfyingCrit.Value.Value);
+            CollectionAssert.AreEqual(itemsSatisfyingCriteria, resultConsumer.Items.Select(e => e.Item.Value).ToList());
+
+            // Make violating item satisfy
+            await collection.ExecuteSync(i => i.Value = 109, itemViolatingCrit.Key);
+            itemsSatisfyingCriteria.Add(109);
+            CollectionAssert.AreEqual(itemsSatisfyingCriteria, resultConsumer.Items.Select(e => e.Item.Value).ToList());
         }
-
     }
 }
