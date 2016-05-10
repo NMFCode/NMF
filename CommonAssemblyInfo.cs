de4dd9ab1a8a51a7a16e25e4d0b59d535866e10e
georghinkel <hinkel@fzi.de>
2016-05-10 09:30:04 +0200

    Version change to 1.0.5

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Resources;
 using System.Runtime.InteropServices;
 [assembly: AssemblyProduct(".NET Modelling Framework")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.4")]
-[assembly: AssemblyFileVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.5")]
 [assembly: NeutralResourcesLanguage("en")]
