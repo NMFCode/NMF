f20bf8fc12918411d00eb34a25f7b1369af9de5f
Georg Hinkel <georg.hinkel@gmail.com>
2016-08-09 22:29:39 +0200

    Added new optimizations project

---
+++ Optimizations/Tests/Optimizations.Tests/MockedBenchmark.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMF.Optimizations.Tests
+{
+    class MockedBenchmark : IBenchmark<string>
+    {
+        private int counter;
+        private Dictionary<string, Func<int, double>> metrics;
+
+        public MockedBenchmark(Func<int, double> time = null, Func<int, double> memory = null, Func<int, double> throughput = null)
+        {
+            metrics = new Dictionary<string, Func<int, double>>();
+            if (time != null) metrics.Add("Time", time);
+            if (memory != null) metrics.Add("Memory", memory);
+            if (throughput != null) metrics.Add("Throughput", throughput);
+        }
+
+        public MockedBenchmark(Dictionary<string, Func<int, double>> metrics)
+        {
+            this.metrics = metrics ?? new Dictionary<string, Func<int, double>>();
+        }
+
+        public IDictionary<string, double> MeasureConfiguration(string configuration)
+        {
+            var results = new Dictionary<string, double>();
+            foreach (var m in metrics)
+            {
+                results.Add(m.Key, m.Value(counter));
+            }
+            counter++;
+            return results;
+        }
+
+        public IEnumerable<MeasuredConfiguration<string>> CreateMeasurements(int n)
+        {
+            var measurements = new List<MeasuredConfiguration<string>>();
+            counter = 0;
+            for (int i = 0; i < n; i++)
+            {
+                var name = "Measurement" + counter.ToString();
+                measurements.Add(new MeasuredConfiguration<string>(name, MeasureConfiguration(name)));
+            }
+            return measurements;
+        }
+    }
+}
---
+++ Optimizations/Tests/Optimizations.Tests/ParetoFilterTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NMF.Optimizations.Tests
+{
+    [TestClass]
+    public class ParetoFilterTests
+    {
+        [TestMethod]
+        public void Pareto_OneDimension_SmallerIsBetter()
+        {
+            var benchmark = new MockedBenchmark(
+                time: n => 10.0 * n + 30,
+                memory: n => 20.0 * n + 10);
+
+            var pareto = new ParetoFilter<string>(new Dictionary<string, DimensionRating>()
+            {
+                { "Time", DimensionRating.SmallerIsBetter }
+            });
+
+            var result = pareto.Filter(benchmark.CreateMeasurements(20)).ToList();
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(30.0, result.Single().Measurements["Time"]);
+        }
+
+        [TestMethod]
+        public void Pareto_OneDimension_BiggerIsBetter()
+        {
+            var benchmark = new MockedBenchmark(
+                time: n => 10.0 * n + 30,
+                memory: n => 20.0 * n + 10);
+
+            var pareto = new ParetoFilter<string>(new Dictionary<string, DimensionRating>()
+            {
+                { "Time", DimensionRating.BiggerIsBetter }
+            });
+
+            var result = pareto.Filter(benchmark.CreateMeasurements(20)).ToList();
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(220.0, result.Single().Measurements["Time"]);
+        }
+
+        [TestMethod]
+        public void Pareto_TwoDimension_Correlated()
+        {
+            var benchmark = new MockedBenchmark(
+                time: n => 10.0 * n + 30,
+                memory: n => 20.0 * n + 10);
+
+            var pareto = new ParetoFilter<string>(new Dictionary<string, DimensionRating>()
+            {
+                { "Time", DimensionRating.SmallerIsBetter },
+                { "Memory", DimensionRating.SmallerIsBetter }
+            });
+
+            var result = pareto.Filter(benchmark.CreateMeasurements(20)).ToList();
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(30.0, result.Single().Measurements["Time"]);
+        }
+
+        [TestMethod]
+        public void Pareto_TwoDimension_NotCorrelated()
+        {
+            var benchmark = new MockedBenchmark(
+                time: n => n < 25 ? 10.0 * n + 30
+                                  : 10.0 * (n-25) + 10,
+                memory: n => n < 25 ? 20.0 * n + 10
+                                    : 30.0 * (n-25) + 20);
+
+            var pareto = new ParetoFilter<string>(new Dictionary<string, DimensionRating>()
+            {
+                { "Time", DimensionRating.SmallerIsBetter },
+                { "Memory", DimensionRating.BiggerIsBetter }
+            });
+
+            var result = pareto.Filter(benchmark.CreateMeasurements(50)).ToList();
+
+            Assert.AreEqual(25, result.Count);
+        }
+    }
+}
