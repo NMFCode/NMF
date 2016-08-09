31502e513e8c3223966467e2929f0d32666fda83
Manuel Leßmann <lessmann.manuel@gmail.com>
2016-08-09 15:45:32 +0200

    Added INotificationResult. OnDemand execution will not work until ExecutionContext is added.

---
+++ Expressions/Expressions/Public/INotificationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NMF.Expressions
+{
+    public interface INotificationResult
+    {
+        INotifiable Source { get; }
+
+        bool Changed { get; }
+    }
+}
