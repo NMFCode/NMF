355087ebd2d1eb9df188195a54c5d1548bccc463
Georg Hinkel <georg.hinkel@gmail.com>
2016-08-09 22:30:40 +0200

    Added new Optimizations project

---
+++ Optimizations/Optimizations/IMeasurementFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NMF.Optimizations
+{
+    public interface IMeasurementFilter<T>
+    {
+        IEnumerable<MeasuredConfiguration<T>> Filter(IEnumerable<MeasuredConfiguration<T>> measurements);
+    }
+}
