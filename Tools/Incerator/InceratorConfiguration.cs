a3ef2bae7902d5905276020180590fdbde19b624
Georg Hinkel <georg.hinkel@gmail.com>
2016-08-09 22:30:16 +0200

    Added new incerator project

---
+++ Tools/Incerator/InceratorConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace NMF.Incerator
+{
+    public class InceratorConfiguration
+    {
+        [Option('m', "model", HelpText = "The model on which incerator should operate", Required = true)]
+        public string Model { get; set; }
+
+        [Option('s', "change-sequence", HelpText = "The change sequence for which the analysis should be optimized", Required = true)]
+        public string ChangeSequence { get; set; }
+
+        [Option('c', "configuration", HelpText = "The change configuration", Required = true)]
+        public string Configuration { get; set; }
+
+        [Option('a', "assembly", HelpText = "The assembly that should be optimized", Required = true)]
+        public string Assembly { get; set; }
+
+        [Option('t', "type", HelpText = "The analysis type that should be optímized", Required = true)]
+        public string Type { get; set; }
+
+        [ValueOption(0)]
+        public OperationMode Mode { get; set; }
+
+
+        public string GetHelp()
+        {
+            return HelpText.AutoBuild(this);
+        }
+    }
+
+    public enum OperationMode
+    {
+        Record,
+        Optimize
+    }
+}
---
+++ Tools/Incerator/Program.cs
@@ -0,0 +1,53 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMF.Incerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InceratorConfiguration options = new InceratorConfiguration();
+            if (Parser.Default.ParseArguments(args, options))
+            {
+#if DEBUG
+                RunIncerator(options);
+#else
+                try
+                {
+                    RunIncerator(options);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+#endif
+            }
+            else
+            {
+                Console.WriteLine("You are using me wrongly!");
+                Console.WriteLine("Usage: Incerator optimize [options]");
+                Console.WriteLine(options.GetHelp());
+            }
+        }
+
+        private static void RunIncerator(InceratorConfiguration options)
+        {
+            switch (options.Mode)
+            {
+                case OperationMode.Record:
+                    break;
+                case OperationMode.Optimize:
+                    break;
+                default:
+                    Console.WriteLine("Operation mode {0} is unknown.", options.Mode);
+                    Environment.Exit(1);
+                    break;
+            }
+        }
+    }
+}
