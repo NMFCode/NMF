c80d2f1ff35a878ff4f97a6053ac614508f61733
Manuel Leßmann <lessmann.manuel@gmail.com>
2016-08-15 21:27:54 +0200

    Migrated INotifyEnumerable and ObservableEnumerable.

--- Expressions/Expressions.Linq/Facade/ObservableCollectionProxy.cs
+++ Expressions/Expressions.Linq/Facade/ObservableCollectionProxy.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace NMF.Expressions.Linq
 {
-    internal sealed class ObservableCollectionProxy<T> : ObservableEnumerable<T>, INotifyCollection<T>
+    public sealed class ObservableCollectionProxy<T> : ObservableEnumerable<T>, INotifyCollection<T>
     {
         private readonly CollectionChangeListener<T> listener;
         private IEnumerable<T> inner;
