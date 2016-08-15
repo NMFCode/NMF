dbb994272ed67e11280b07f84864f8c579c52be1
Manuel Leßmann <lessmann.manuel@gmail.com>
2016-08-15 21:52:00 +0200

    Migrated ObservableDistinct.

---
+++ Expressions/Expressions.Linq/Linq/CollectionChangedNotificationResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NMF.Expressions.Linq
+{
+    internal class CollectionChangedNotificationResult<T> : INotificationResult
+    {
+        public bool Changed { get { return true; } }
+
+        public INotifiable Source { get; private set; }
+
+        public List<T> AddedItems { get; private set; }
+
+        public List<T> RemovedItems { get; private set; }
+
+        public bool IsReset { get; private set; }
+
+        private CollectionChangedNotificationResult(INotifiable source, List<T> addedItems, List<T> removedItems, bool isReset)
+        {
+            Source = source;
+            AddedItems = addedItems;
+            RemovedItems = removedItems;
+            IsReset = isReset;
+        }
+
+        public static CollectionChangedNotificationResult<T> Reset(INotifiable source)
+        {
+            return new CollectionChangedNotificationResult<T>(source, null, null, true);
+        }
+
+        public static CollectionChangedNotificationResult<T> AddRemove(INotifiable source, List<T> addedItems, List<T> removedItems)
+        {
+            if (addedItems != null && addedItems.Count == 0)
+                addedItems = null;
+            if (removedItems != null && removedItems.Count == 0)
+                removedItems = null;
+            return new CollectionChangedNotificationResult<T>(source, addedItems, removedItems, false);
+        }
+    }
+}
