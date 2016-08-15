a10ab9024c9a9b701a65ca0b355591b1d1d92f4f
Manuel Leßmann <lessmann.manuel@gmail.com>
2016-08-15 22:32:23 +0200

    Migrated ObservableOfType.

--- Expressions/Expressions.Linq/Linq/CollectionChangedNotificationResult.cs
+++ Expressions/Expressions.Linq/Linq/CollectionChangedNotificationResult.cs
@@ -1,38 +1,45 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace NMF.Expressions.Linq
 {
-    internal abstract class CollectionChangedNotificationResult : INotificationResult
+    internal interface ICollectionChangedNotificationResult : INotificationResult
+    {
+        bool IsReset { get; }
+
+        IList AddedItems { get; }
+
+        IList RemovedItems { get; }
+
+        new INotifiable Source { get; set; }
+    }
+
+    internal class CollectionChangedNotificationResult<T> : ICollectionChangedNotificationResult
     {
         public bool Changed { get { return true; } }
 
-        public INotifiable Source { get; private set; }
+        public INotifiable Source { get; set; }
 
         public bool IsReset { get; private set; }
 
-        protected CollectionChangedNotificationResult(INotifiable source, bool isReset)
+        public List<T> AddedItems { get; private set; }
+
+        public List<T> RemovedItems { get; private set; }
+
+        IList ICollectionChangedNotificationResult.AddedItems { get { return AddedItems; } }
+
+        IList ICollectionChangedNotificationResult.RemovedItems { get { return RemovedItems; } }
+
+        private CollectionChangedNotificationResult(INotifiable source, bool isReset)
         {
             Source = source;
             IsReset = isReset;
         }
 
-        public CollectionChangedNotificationResult Forward(INotifiable newSource)
-        {
-            Source = newSource;
-            return this;
-        }
-    }
-
-    internal class CollectionChangedNotificationResult<T> : CollectionChangedNotificationResult
-    {
-        public List<T> AddedItems { get; private set; }
-
-        public List<T> RemovedItems { get; private set; }
-
-        public CollectionChangedNotificationResult(INotifiable source, List<T> addedItems = null, List<T> removedItems = null) : base(source, false)
+        public CollectionChangedNotificationResult(INotifiable source, List<T> addedItems, List<T> removedItems) : this(source, false)
         {
             if (addedItems != null && addedItems.Count == 0)
                 AddedItems = null;
@@ -48,6 +55,6 @@ namespace NMF.Expressions.Linq
                 throw new ArgumentException("A collection change that is not a reset must have at least one add or one remove.");
         }
 
-        public CollectionChangedNotificationResult(INotifiable source) : base(source, true) { }
+        public CollectionChangedNotificationResult(INotifiable source) : this(source, true) { }
     }
 }
