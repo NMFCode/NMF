388543846519a3a08aa87a5b3d72bc4b98158642
Manuel Leßmann <lessmann.manuel@gmail.com>
2016-08-19 10:52:53 +0200

    Implemented ObservableCollectionProxy. Still need collection change information from the underlying collection in Notify().

--- Expressions/Expressions.Linq/Facade/ObservableCollectionProxy.cs
+++ Expressions/Expressions.Linq/Facade/ObservableCollectionProxy.cs
@@ -8,68 +8,67 @@ namespace NMF.Expressions.Linq
 {
     public sealed class ObservableCollectionProxy<T> : ObservableEnumerable<T>, INotifyCollection<T>
     {
-        private IEnumerable<T> Inner { get; set; }
+        private readonly IExecutionContext context;
+        private IEnumerable<T> inner;
+
+        public override IEnumerable<INotifiable> Dependencies { get { return Enumerable.Empty<INotifiable>(); } }
 
         public ObservableCollectionProxy(IEnumerable<T> inner)
         {
             if (inner == null) throw new ArgumentNullException("inner");
 
-            Inner = inner;
-
-            Attach();
+            this.inner = inner;
+            this.context = ExecutionEngine.Current.Context;
         }
 
         public override IEnumerator<T> GetEnumerator()
         {
-            return Inner.GetEnumerator();
+            return inner.GetEnumerator();
         }
 
-        protected override void AttachCore()
+        protected override void OnAttach()
         {
-            var notifier = Inner as INotifyCollectionChanged;
-            if (notifier != null)
-            {
-                notifier.CollectionChanged += InnerCollectionChanged;
-            }
+            var notifiable = inner as INotifyCollectionChanged;
+            if (notifiable != null)
+                context.AddChangeListener(this, notifiable);
         }
 
-        private void InnerCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        protected override void OnDetach()
         {
-            OnCollectionChanged(e);
+            var notifiable = inner as INotifyCollectionChanged;
+            if (notifiable != null)
+                context.RemoveChangeListener(this, notifiable);
         }
 
-        protected override void DetachCore()
+        public override INotificationResult Notify(IList<INotificationResult> sources)
         {
-            var notifier = Inner as INotifyCollectionChanged;
-            if (notifier != null)
-            {
-                notifier.CollectionChanged -= InnerCollectionChanged;
-            }
+            //TODO Need information from the NotifyCollectionChangedEventArgs here.
+            return new CollectionChangedNotificationResult<T>(this);
         }
 
         public override bool Contains(T item)
         {
-            return Inner.Contains(item);
+            return inner.Contains(item);
         }
 
         public override int Count
         {
             get
             {
-                return Inner.Count();
+                return inner.Count();
             }
         }
 
         void ICollection<T>.Add(T item)
         {
-            var coll = Inner as ICollection<T>;
+            var coll = inner as ICollection<T>;
             if (coll == null || coll.IsReadOnly) throw new InvalidOperationException("Source is not a collection or is read-only");
             coll.Add(item);
         }
 
         void ICollection<T>.Clear()
         {
-            var coll = Inner as ICollection<T>;
+            var coll = inner as ICollection<T>;
             if (coll == null || coll.IsReadOnly) throw new InvalidOperationException("Source is not a collection or is read-only");
             var list = new List<T>(this);
             if (list.Count == coll.Count)
@@ -89,14 +88,14 @@ namespace NMF.Expressions.Linq
         {
             get
             {
-                var collection = Inner as ICollection<T>;
+                var collection = inner as ICollection<T>;
                 return collection == null || collection.IsReadOnly;
             }
         }
 
         bool ICollection<T>.Remove(T item)
         {
-            var coll = Inner as ICollection<T>;
+            var coll = inner as ICollection<T>;
             if (coll == null || coll.IsReadOnly) throw new InvalidOperationException("Source is not a collection or is read-only");
             return coll.Remove(item);
         }
