a7da531dd59cb6e1b56cd5ba3193fc2e4746c1c3
Manuel Leßmann <lessmann.manuel@gmail.com>
2016-08-26 18:42:53 +0200

    Properly implemented NotifyCollection. It registers a collection change listener for itself.

--- Expressions/Expressions.Linq/Facade/NotifyCollection.cs
+++ Expressions/Expressions.Linq/Facade/NotifyCollection.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using NMF.Expressions.Linq;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 
@@ -8,13 +10,25 @@ namespace NMF.Expressions
 {
     public class NotifyCollection<T> : ObservableCollection<T>, INotifyEnumerable<T>, INotifyCollection<T>
     {
-        private ShortList<INotifiable> successors = new ShortList<INotifiable>();
+        private readonly ShortList<INotifiable> successors = new ShortList<INotifiable>();
+        private readonly IExecutionContext context = ExecutionEngine.Current.Context;
 
         public virtual IEnumerable<INotifiable> Dependencies { get { return Enumerable.Empty<INotifiable>(); } }
 
         public ExecutionMetaData ExecutionMetaData { get; } = new ExecutionMetaData();
 
         public IList<INotifiable> Successors { get { return successors; } }
+
+        public NotifyCollection()
+        {
+            successors.CollectionChanged += (obj, e) =>
+            {
+                if (successors.Count == 0)
+                    Detach();
+                else if (e.Action == NotifyCollectionChangedAction.Add && successors.Count == 1)
+                    Attach();
+            };
+        }
 
         public void Dispose()
         {
@@ -24,11 +38,20 @@ namespace NMF.Expressions
 
         public virtual INotificationResult Notify(IList<INotificationResult> sources)
         {
-            return new UnchangedNotificationResult(this);
+            //TODO Need information from the NotifyCollectionChangedEventArgs here.
+            return new CollectionChangedNotificationResult<T>(this);
         }
 
-        protected virtual void Dispose(bool disposing)
+        protected virtual void Dispose(bool disposing) { }
+
+        private void Attach()
         {
+            context.AddChangeListener(this, this);
+        }
+
+        private void Detach()
+        {
+            context.RemoveChangeListener(this, this);
         }
     }
 }
