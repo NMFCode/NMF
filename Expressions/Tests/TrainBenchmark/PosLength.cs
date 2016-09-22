5203ebe21832cc925a272bf6048b460239fa2df4
Manuel Leßmann <lessmann.manuel@gmail.com>
2016-09-22 15:43:05 +0200

    Fixed the PosLength benchmark.

--- Expressions/Tests/TrainBenchmark/PosLength.cs
+++ Expressions/Tests/TrainBenchmark/PosLength.cs
@@ -13,15 +13,15 @@ namespace TrainBenchmark
     public class PosLength : TrainCase<Segment, Segment>
     {
         public override Func<RailwayContainer, INotifyEnumerable<Segment>> Query =>
-            rc => rc.Descendants().OfType<Segment>().Where(seg => seg.Length < 0).AsNotifiable();
+            rc => rc.Descendants().OfType<Segment>().Where(seg => seg.Length <= 0).AsNotifiable();
 
         public override Action<Segment> Repair =>
-            segment => segment.Length = 0;
+            segment => segment.Length = -segment.Length + 1;
 
         public override Func<RailwayContainer, INotifyEnumerable<Segment>> InjectSelector =>
-            rc => rc.Descendants().OfType<Segment>().Where(seg => seg.Length >= 0).AsNotifiable();
+            rc => rc.Descendants().OfType<Segment>().Where(seg => seg.Length > 0).AsNotifiable();
 
         public override Action<Segment> Inject =>
-            segment => segment.Length = -segment.Length + 1;
+            segment => segment.Length = 0;
     }
 }
