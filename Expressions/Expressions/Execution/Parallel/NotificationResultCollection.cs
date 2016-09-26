26d9456e988dff47f4835d2c3a115865485bbf6b
Manuel Leßmann <lessmann.manuel@gmail.com>
2016-09-26 20:13:26 +0200

    Implemented NotificationResultCollection.

---
+++ Expressions/Expressions/Execution/Parallel/NotificationResultCollection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace NMF.Expressions
+{
+    internal class NotificationResultCollection
+    {
+        private INotificationResult[] array = new INotificationResult[2];
+        private int count = -1;
+
+        public int Count { get { return count + 1; } }
+
+        public IList<INotificationResult> Values { get { return new ArraySegment<INotificationResult>(array, 0, Count); } }
+
+        public void Add(INotificationResult item)
+        {
+            INotificationResult[] oldArray, newArray;
+            var index = Interlocked.Increment(ref count);
+
+            do
+            {
+                oldArray = array;
+                newArray = array;
+                EnsureCapacity(ref newArray, index);
+                newArray[index] = item;
+            } while (Interlocked.CompareExchange(ref array, newArray, oldArray) != newArray);
+        }
+
+        public void UnsafeAdd(INotificationResult item)
+        {
+            count++;
+            EnsureCapacity(ref array, count);
+            array[count] = item;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < Count; i++)
+                array[i] = null;
+            count = -1;
+        }
+
+        private void EnsureCapacity(ref INotificationResult[] array, int capacity)
+        {
+            if (capacity >= array.Length)
+            {
+                var newArray = new INotificationResult[array.Length * 2];
+                Array.Copy(array, newArray, array.Length);
+                array = newArray;
+            }
+        }
+    }
+}
