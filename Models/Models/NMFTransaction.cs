f42299d24d39a0aafbd33079a3c50e285a3c153c
Tizian <tizian.hoffmann@gmail.com>
2016-09-30 16:18:32 +0200

    Added transaction API

---
+++ Models/Models/NMFTransaction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMF.Expressions;
+
+namespace NMF.Models
+{
+    class NMFTransaction : IDisposable
+    {
+        private bool committed = false;
+        private readonly ExecutionEngine engine = ExecutionEngine.Current;
+
+        public NMFTransaction()
+        {
+            engine.BeginTransaction();
+        }
+        public void Commit()
+        {
+            engine.CommitTransaction();
+            committed = true;
+            throw new NotImplementedException();
+        }
+        public void Rollback()
+        {
+            throw new NotImplementedException();
+            
+        }
+        public void Dispose()
+        {
+            if (!committed)
+            {
+                Rollback();
+            }
+        }
+
+    }
+}
