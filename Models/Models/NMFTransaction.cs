1c1325df07cad5157759ce0fc62e589b08aa58c5
Tizian <tizian.hoffmann@gmail.com>
2016-10-01 18:53:20 +0200

    Added optional use of the TransactionScope class.

--- Models/Models/NMFTransaction.cs
+++ Models/Models/NMFTransaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Transactions;
 using NMF.Expressions;
 using NMF.Models.Evolution;
 
@@ -10,17 +11,21 @@ namespace NMF.Models
         private bool _committed;
         private readonly ExecutionEngine _engine = ExecutionEngine.Current;
         private readonly ModelChangeRecorder _recorder = new ModelChangeRecorder();
+        //private TransactionScope _scope;
 
         public NMFTransaction(IModelElement rootElement)
         {
             if (rootElement == null) throw new ArgumentNullException(nameof(rootElement));
             _recorder.Start(rootElement);
+            //_scope = new TransactionScope();
             _engine.BeginTransaction();
         }
         public void Commit()
         {
+            //if (_scope == null) throw new InvalidOperationException();
             _engine.CommitTransaction();
             _committed = true;
+            //_scope.Complete();
         }
         public void Rollback()
         {
@@ -35,6 +40,11 @@ namespace NMF.Models
         {
             if (!_committed)
                 Rollback();
+            //if (_scope != null)
+            //{
+            //    _scope.Dispose();
+            //    _scope = null;
+            //}
         }
 
     }
