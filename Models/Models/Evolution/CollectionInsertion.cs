4d2d84901285830db900559606051560627b1db3
Tizian <tizian.hoffmann@gmail.com>
2016-10-08 15:36:25 +0200

    Added classes to handle cases where collections other than lists (i.e. sets) are used.

---
+++ Models/Models/Evolution/CollectionInsertion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMF.Models.Repository;
+
+namespace NMF.Models.Evolution
+{
+    //TODO serialize
+    public abstract class CollectionInsertionBase<T> : IModelChange
+    {
+        public Uri AbsoluteUri { get; }
+        public void Apply(IModelRepository repository)
+        {
+            throw new NotImplementedException();
+        }
+        //TODO constructor
+        public void Invert(IModelRepository repository)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
