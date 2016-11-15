78098e8c980c133d0ff06c4b5cab6d95573b5b4c
Tizian <tizian.hoffmann@gmail.com>
2016-11-16 00:41:04 +0100

    WIP commit

---
+++ Models/Models/Evolution/CollectionReset.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMF.Models.Repository;
+
+namespace NMF.Models.Evolution
+{
+    public class CollectionReset<T> : IModelChange
+    {
+        private ModelChangeRecorder _recorder;
+
+        public Uri AbsoluteUri { get; }
+
+        public string CollectionPropertyName { get; set; }
+
+        public CollectionReset(Uri absoluteUri, string collectionPropertyName, ModelChangeRecorder recorder)
+        {
+            AbsoluteUri = absoluteUri;
+            CollectionPropertyName = collectionPropertyName;
+            _recorder = recorder;
+        }
+        public void Apply(IModelRepository repository)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Invert(IModelRepository repository)
+        {
+            var items = _recorder.GetItemsBeforeReset(this); //TODO überlegen, außerdem rausfinden ob die ganze Klasse nach Manuels Meinung überhaupt Sinn macht
+            var parent = repository.Resolve(AbsoluteUri);
+            var property = parent.GetType().GetProperty(CollectionPropertyName);
+            var coll = property.GetValue(parent, null) as ICollection<T>;
+
+            coll.Clear();
+            foreach (var item in items.Cast<T>().ToList())
+            {
+                coll.Add(item);
+            }
+        }
+    }
+}
