fd1e100624af0d76860d67431b36f26d8e1b9024
Georg Hinkel <hinkel@fzi.de>
2016-11-25 11:08:49 +0100

    Create unit tests to generate code for multiple metamodels

---
+++ Transformations/Tests/CodeGenerationTests/CodeGenerationTest.cs
@@ -0,0 +1,102 @@
+﻿using NMF.Collections.ObjectModel;
+using NMF.Expressions;
+using NMF.Expressions.Linq;
+using NMF.Models.Meta;
+using NMF.Serialization;
+using NMF.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMF.CodeGenerationTests
+{
+    public static class CodeGenerationTest
+    {
+        private static object codeGenLock = new object();
+
+        public static int GenerateAndCompile(INamespace ns, out string error, out string log)
+        {
+            lock (codeGenLock)
+            {
+                var path = Path.GetTempFileName();
+                File.Delete(path);
+                Directory.CreateDirectory(path);
+
+                try
+                {
+                    var projectFile = Path.Combine(path, "project.csproj");
+                    var codeFile = Path.Combine(path, "code.cs");
+
+                    var code = MetaFacade.CreateCode(ns, "Test");
+                    MetaFacade.GenerateCode(code, new Microsoft.CSharp.CSharpCodeProvider(), codeFile, false);
+
+                    File.WriteAllText(projectFile, GenerateProjectFile(codeFile));
+
+                    var startInfo = new ProcessStartInfo("MSBuild.exe", projectFile);
+                    startInfo.CreateNoWindow = true;
+                    startInfo.ErrorDialog = false;
+                    startInfo.UseShellExecute = false;
+                    startInfo.RedirectStandardError = true;
+                    startInfo.RedirectStandardOutput = true;
+                    startInfo.WorkingDirectory = path;
+
+                    var buildJob = Process.Start(startInfo);
+                    string buildLog = null;
+
+                    buildJob.ErrorDataReceived += (o, e) =>
+                    {
+                        buildLog += e.Data;
+                        Debug.WriteLine(e.Data);
+                    };
+                    log = null;
+                    var line = buildJob.StandardOutput.ReadLine();
+                    while (line != null)
+                    {
+                        log += line;
+                        line = buildJob.StandardOutput.ReadLine();
+                    }
+                    buildJob.WaitForExit();
+                    error = buildLog;
+                    return buildJob.ExitCode;
+                }
+                finally
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+        }
+
+        private static string GenerateProjectFile(params string[] codeFiles)
+        {
+            var compileFilesSnippet = string.Concat(codeFiles.Select(file => Environment.NewLine + string.Format("   <Compile Include=\"{0}\" />", Path.GetFileName(file))));
+            var collectionsPath = (typeof(ObservableSet<>)).Assembly.Location;
+            var expressionsPath = (typeof(INotifyExpression)).Assembly.Location;
+            var expressionsLinqPath = (typeof(ExpressionExtensions)).Assembly.Location;
+            var modelsPath = typeof(NMF.Models.Model).Assembly.Location;
+            var serializationsPath = typeof(XmlSerializer).Assembly.Location;
+            var utilitiesPath = typeof(MemoizedFunc<,>).Assembly.Location;
+
+            using (var projectTemplateStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("NMF.CodeGenerationTests.ProjectTemplate.csproj"))
+            {
+                using (var sr = new StreamReader(projectTemplateStream))
+                {
+                    var projectTemplate = sr.ReadToEnd();
+
+                    return projectTemplate
+                        .Replace("%NMFCollectionsPath%", collectionsPath)
+                        .Replace("%NMFExpressionsPath%", expressionsPath)
+                        .Replace("%NMFExpressionsLinqPath%", expressionsLinqPath)
+                        .Replace("%NMFModelsPath%", modelsPath)
+                        .Replace("%NMFSerializationPath%", serializationsPath)
+                        .Replace("%NMFUtilitiesPath%", utilitiesPath)
+                        .Replace("%CompileFiles%", compileFilesSnippet);
+                }
+            }
+        }
+    }
+}
---
+++ Transformations/Tests/CodeGenerationTests/ModelTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NMF.Models.Repository;
+using NMF.Interop.Ecore;
+
+namespace NMF.CodeGenerationTests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void ClassModelGeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("Class.ecore");
+        }
+
+        [TestMethod]
+        public void ArchitectureCRAModelGeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("architectureCRA.ecore");
+        }
+
+        [TestMethod]
+        public void FamiliesModelGeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("Families.ecore");
+        }
+
+        [TestMethod]
+        public void PersonsModelGeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("Persons.ecore");
+        }
+
+        [TestMethod]
+        public void RailwayModelGeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("railway.ecore");
+        }
+
+        [TestMethod]
+        public void RelationalModelGeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("Relational.ecore");
+        }
+
+        [TestMethod]
+        public void ABBModelGeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("ABB.ecore");
+        }
+
+        [TestMethod]
+        public void Iso61850GeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("61850.ecore");
+        }
+
+        [TestMethod]
+        public void CosemModelGeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("COSEM.ecore");
+        }
+
+        [TestMethod]
+        public void SmartGridSchemaGeneratesSuccessfully()
+        {
+            GenerateAndAssertEcore("schema.ecore");
+        }
+
+        private void GenerateAndAssertEcore(string modelPath)
+        {
+            string log;
+            string errorLog;
+            var package = EcoreInterop.LoadPackageFromFile(modelPath);
+            var ns = EcoreInterop.Transform2Meta(package);
+            var result = CodeGenerationTest.GenerateAndCompile(ns, out errorLog, out log);
+            Assert.AreEqual(0, result, log + errorLog);
+        }
+    }
+}
