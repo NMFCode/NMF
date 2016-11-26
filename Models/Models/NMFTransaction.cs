0f087743b3e35ae30df24021049cdb06858a8518
Tizian <tizian.hoffmann@googlemail.com>
2016-11-26 22:53:56 +0100

    Cleanup.

--- Models/Models/NMFTransaction.cs
+++ Models/Models/NMFTransaction.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Transactions;
 using NMF.Expressions;
 using NMF.Models.Evolution;
 using NMF.Models.Repository;
@@ -13,22 +11,18 @@ namespace NMF.Models
         private readonly ExecutionEngine _engine = ExecutionEngine.Current;
         private readonly ModelChangeRecorder _recorder = new ModelChangeRecorder(true);
         private IModelRepository _repository;
-        //private TransactionScope _scope;
 
         public NMFTransaction(IModelElement rootElement)
         {
             if (rootElement == null) throw new ArgumentNullException(nameof(rootElement));
             _repository = rootElement.Model.Repository;
             _recorder.Start(rootElement);
-            //_scope = new TransactionScope();
             _engine.BeginTransaction();
         }
         public void Commit()
         {
-            //if (_scope == null) throw new InvalidOperationException();
             _engine.CommitTransaction();
             _committed = true;
-            //_scope.Complete();
         }
         public void Rollback()
         {
@@ -44,11 +38,6 @@ namespace NMF.Models
         {
             if (!_committed)
                 Rollback();
-            //if (_scope != null)
-            //{
-            //    _scope.Dispose();
-            //    _scope = null;
-            //}
         }
 
     }
