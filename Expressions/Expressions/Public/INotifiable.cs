621e884c6e506a3366d830e760c2049c5bd80825
Georg Hinkel <hinkel@fzi.de>
2017-10-06 16:16:15 +0200

    migrate to MSTest v2

--- Expressions/Expressions/Public/INotifiable.cs
+++ Expressions/Expressions/Public/INotifiable.cs
@@ -33,4 +33,42 @@ namespace NMF.Expressions
         /// </summary>
         ExecutionMetaData ExecutionMetaData { get; }
     }
+
+
+    /// <summary>
+    /// Represents a node in a graph which can notify its successors
+    /// and gets notified by its dependencies.
+    /// </summary>
+    public abstract class Notifiable : INotifiable, IDisposable
+    {
+        private ISuccessorList successors = NotifySystem.DefaultSystem.CreateSuccessorList();
+        private ExecutionMetaData metadata = new ExecutionMetaData();
+
+        /// <summary>
+        /// The nodes that will get notified by this node.
+        /// </summary>
+        public ISuccessorList Successors { get { return successors;} }
+
+        /// <summary>
+        /// Nodes that notify this node.
+        /// </summary>
+        public abstract IEnumerable<INotifiable> Dependencies { get; }
+
+        /// <summary>
+        /// Gets called when one of the dependencies signals a notification.
+        /// </summary>
+        /// <param name="source">Contains information about what triggered this notification.</param>
+        /// <returns>An object describing the changes that happened in this notification.</returns>
+        public abstract INotificationResult Notify(IList<INotificationResult> sources);
+
+        public void Dispose()
+        {
+            Successors.UnsetAll();
+        }
+
+        /// <summary>
+        /// Used by the execution engine during incremental execution.
+        /// </summary>
+        public ExecutionMetaData ExecutionMetaData { get { return metadata; } }
+    }
 }
