ec06ffd82537a2dee18d79cd2d74798b8363b55a
Georg Hinkel <hinkel@fzi.de>
2018-01-12 11:00:05 +0100

    Added new unit tests

---
+++ IntegrationTests/IncrementalizationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+using NMF.Models;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IncrementalizationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IncrementalizationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("38a5ef80-6411-4522-9810-f1bf2fcbc8cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ModelMetadata("https://gitlab.upseil.com/lennart/streaming-model-analysis/debs2016/social-network", "NMF.Expressions.Tests.social_network.nmeta")]
---
+++ IntegrationTests/IncrementalizationTests/SocialNetworkTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NMF.Expressions.Linq;
+using NMF.Expressions.Tests.SocialNetwork;
+using NMF.Models;
+
+namespace NMF.Expressions.Tests
+{
+    [TestClass]
+    public class SocialNetworkTests
+    {
+        [TestMethod]
+        public void TestPostScore()
+        {
+            var post = new Post
+            {
+                Active = true,
+                Content = "Test",
+                Score = 10,
+                Id = "42"
+            };
+            var comment1 = new Comment
+            {
+                Content = "Foo",
+                Id = "42.1",
+                Score = 10,
+                Post = post
+            };
+            var comment2 = new Comment
+            {
+                Content = "Foo",
+                Id = "42.2",
+                Score = 10,
+                Post = post
+            };
+            post.Comments.Add(comment1);
+            post.Comments.Add(comment2);
+            var totalScore = Observable.Expression(() => post.Score + post.Descendants().OfType<IComment>().Sum(c => c.Score));
+
+            Assert.AreEqual(30, totalScore.Value);
+
+            var currentValue = 29;
+            var oldValue = 30;
+            var updated = false;
+
+            totalScore.ValueChanged += (o, e) =>
+            {
+                Assert.AreEqual(e.OldValue, oldValue);
+                Assert.AreEqual(e.NewValue, currentValue);
+                updated = true;
+            };
+
+            comment1.Score = 9;
+
+            Assert.IsTrue(updated);
+            Assert.AreEqual(29, totalScore.Value);
+
+            currentValue = 39;
+            oldValue = 29;
+            updated = false;
+
+            var comment11 = new Comment
+            {
+                Content = "Bullshit",
+                Id = "42.1.1",
+                Score = 10
+            };
+            comment1.Comments.Add(comment11);
+
+            Assert.IsTrue(updated);
+            Assert.AreEqual(39, totalScore.Value);
+
+            currentValue = 38;
+            oldValue = 39;
+            updated = false;
+
+            comment1.Comments[0].Score = 9;
+
+            Assert.IsTrue(updated);
+            Assert.AreEqual(38, totalScore.Value);
+
+            currentValue = 20;
+            oldValue = 38;
+            updated = false;
+
+            post.Comments.Remove(comment1);
+
+            Assert.IsTrue(updated);
+            Assert.AreEqual(20, totalScore.Value);
+
+            currentValue = 19;
+            oldValue = 20;
+            updated = false;
+
+            post.Score = 9;
+
+            Assert.IsTrue(updated);
+            Assert.AreEqual(19, totalScore.Value);
+        }
+    }
+}
