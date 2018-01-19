7b455c49f92f34aa849c93e485f17a6c859051c4
Georg Hinkel <hinkel@fzi.de>
2018-01-19 14:06:41 +0100

    Added an analyzer to find out whether a method is stateless

---
+++ Expressions/Expressions/ExpressionAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace NMF.Expressions
+{
+    public class ExpressionAnalyzer : ExpressionVisitorBase
+    {
+        protected ExpressionAnalyzer() { }
+
+        public static bool IsStateless(LambdaExpression lambdaExpression)
+        {
+            var instance = new ExpressionAnalyzer();
+            instance.Visit(lambdaExpression);
+            return instance.isStateLess;
+        }
+
+        private bool isStateLess = true;
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            if (!node.Member.DeclaringType.IsValueType)
+            {
+                var generated = node.Member.DeclaringType.GetCustomAttributes(typeof(CompilerGeneratedAttribute), false);
+                if (generated == null || generated.Length == 0)
+                {
+                    isStateLess = false;
+                }
+            }
+            return base.VisitMember(node);
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (!node.Method.DeclaringType.IsValueType)
+            {
+                var immutable = node.Method.GetCustomAttributes(typeof(ImmutableMethodAttribute), true);
+                if (immutable == null || immutable.Length > 0)
+                {
+                    isStateLess = false;
+                }
+            }
+            return base.VisitMethodCall(node);
+        }
+    }
+}
