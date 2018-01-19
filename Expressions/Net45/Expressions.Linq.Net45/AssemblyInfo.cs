bb584da7bac8e11fd89bc05daed5e912e35418fb
Georg Hinkel <hinkel@fzi.de>
2018-01-19 15:11:41 +0100

    Support for .NET 4.5 through cross-compilation

---
+++ Expressions/Net45/Expressions.Linq.Net45/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NMF Expressions")]
+[assembly: AssemblyDescription("An expression framework and query syntax implementation that implements INotifyCollectionChanged")]
+[assembly: AssemblyCopyright("Copyright © Georg Hinkel 2013")]
+[assembly: CLSCompliant(true)]
