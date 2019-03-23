3c9ac2db421a957e49ed5cb087f3f722ebc57e34
Georg Hinkel <georg.hinkel@gmail.com>
2019-03-23 21:59:54 +0100

    remove old .net 4.5 projects

--- Expressions/Net45/Expressions.Linq.Net45/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NMF Expressions")]
-[assembly: AssemblyDescription("An expression framework and query syntax implementation that implements INotifyCollectionChanged")]
-[assembly: AssemblyCopyright("Copyright © Georg Hinkel 2013")]
-[assembly: CLSCompliant(true)]
