c79a928038d35d6f3acaf4075181432d52573140
Can Özkan <can@oezkan.me>
2024-05-30 02:22:07 +0200

    feat: log, when server is ready

--- UserInterfaces/NMetaGlspEditor/NMetaGlspEditor.Server/Program.cs
+++ UserInterfaces/NMetaGlspEditor/NMetaGlspEditor.Server/Program.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.WebSockets;
 using NMetaEditor.Language;
-using NMF.Models.Services;
 using NMF.Models.Services.Forms;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,13 +20,20 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseStaticFiles();
-
 app.UseWebSockets();
 app.MapGlspWebSocketServer("/glsp");
+app.MapPropertyServiceWebSocket("/prop");
 app.MapControllers();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.Run();
+app.Start();
+
+var server = app.Services.GetService<IServer>();
+var addressFeature = server.Features.Get<IServerAddressesFeature>();
+var port = addressFeature.Addresses.First().Split(':')[^1];
+
+Console.WriteLine("[GLSP-Server]:Startup completed. Accepting requests on port: " + port);
+
+app.WaitForShutdown();
