2d5ae320ab87c04f0a6efcf8bdd19e408cab7ba0
ahert001 <alexander.hert@student.hs-rm.de>
2025-01-16 13:35:46 +0100

    Add Document Formatting

---
+++ AnyText/AnyText.Lsp/LspServer.Formatting.cs
@@ -0,0 +1,43 @@
+using LspTypes;
+using NMF.AnyText.Metamodel;
+using System;
+using System.Linq;
+using Range = LspTypes.Range;
+
+namespace NMF.AnyText
+{
+    public partial class LspServer
+    {
+        /// <inheritdoc cref="ILspServer.Formatting" />
+        public LspTypes.TextEdit[] Formatting(TextDocumentIdentifier textDocument, FormattingOptions options)
+        {
+            if (!_documents.TryGetValue(textDocument.Uri, out var document))
+                return null;
+            var edits = document.Format(
+                tabsize: options.TabSize,
+                insertSpaces: options.InsertSpaces,
+                insertFinalNewline: options.InsertFinalNewline ?? false,
+                otherOptions: options.OtherOptions,
+                trimFinalNewlines: options.TrimFinalNewlines ?? false,
+                trimTrailingWhitespace: options.TrimTrailingWhitespace ?? false
+            );
+            var lspEdits = LspTypesMapper.MapToLspTextEdits(edits);
+            return lspEdits.ToArray();
+        }
+
+        /// <inheritdoc cref="ILspServer.FormattingRange" />
+        public LspTypes.TextEdit[] FormattingRange(TextDocumentIdentifier textDocument, Range range,
+            FormattingOptions options)
+        {
+            throw new NotImplementedException();
+            if (!_documents.TryGetValue(textDocument.Uri, out var document))
+                return null;
+            var edits = document.Format(AsParsePosition(range.Start), AsParsePosition(range.End), options.TabSize,
+                options.InsertSpaces, options.OtherOptions);
+            var lspEdits = LspTypesMapper.MapToLspTextEdits(edits);
+
+            
+            return lspEdits.ToArray();
+        }
+    }
+}
