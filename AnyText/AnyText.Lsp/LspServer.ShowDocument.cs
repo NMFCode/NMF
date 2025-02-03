5d64f6efc1d40b1eb36abf9aa1c66a8d679f4659
ahert001 <alexander.hert@student.hs-rm.de>
2025-02-03 09:30:20 +0100

    Add ShowMessage and ShowDocument

---
+++ AnyText/AnyText.Lsp/LspServer.ShowDocument.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using LspTypes;
+using Range = LspTypes.Range;
+
+namespace NMF.AnyText
+{
+    public partial class LspServer
+    {
+        private async void ShowDocument(string uri, Range selection = null, bool external = false,
+            bool takeFocus = false)
+        {
+            if (!_clientCapabilities.Window.ShowDocument.Support)
+            {
+                SendLogMessage(MessageType.Warning, "Client does not support ShowDocument.");
+                return;
+            }
+
+            var showDocumentParams = new ShowDocumentParams
+            {
+                Uri = uri,
+                Selection = selection,
+                External = external,
+                TakeFocus = takeFocus
+            };
+            
+            try
+            {
+                var result =
+                    await _rpc.InvokeWithParameterObjectAsync<ShowDocumentResult>(MethodConstants.WindowShowDocument,
+                        showDocumentParams);
+                SendLogMessage(MessageType.Info,
+                    result.Success ? $"Success ShowDocument {uri}" : $"Failed To ShowDocument {uri}");
+            }
+            catch (Exception e)
+            {
+                SendLogMessage(MessageType.Error, e.Message);
+            }
+        }
+    }
+}
---
+++ AnyText/AnyText.Lsp/LspServer.ShowMessage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LspTypes;
+
+namespace NMF.AnyText
+{
+    public partial class LspServer
+    {
+        private async Task<MessageActionItem> ShowMessageRequestAsync(string message, MessageType messageType,
+            IEnumerable<string> titles)
+        {
+            var supportsMessageAction =
+                _clientCapabilities.Window.ShowMessage.AnnotationId.AdditionalPropertiesSupport == true;
+            if (!supportsMessageAction)
+            {
+                SendLogMessage(MessageType.Warning, "Client does not support ShowMessageRequest");
+                return null;
+            }
+
+            var messageParams = new ShowMessageRequestParams
+            {
+                Message = message,
+                Type = (int)messageType,
+                Actions = titles.Select(title => new MessageActionItem
+                {
+                    Title = title
+                }).ToArray()
+            };
+            
+            try
+            {
+                var result =
+                    await _rpc.InvokeWithParameterObjectAsync<MessageActionItem>(Methods.WindowShowMessageRequestName,
+                        messageParams);
+                return result;
+            }
+            catch (Exception e)
+            {
+                SendLogMessage(MessageType.Error, e.Message);
+                return null;
+            }
+        }
+
+        private void ShowMessageNotify(string message, MessageType messageType)
+        {
+            var messageParams = new ShowMessageParams
+            {
+                Message = message,
+                MessageType = messageType
+            };
+
+            _rpc.NotifyWithParameterObjectAsync(Methods.WindowShowMessageName, messageParams);
+        }
+    }
+}
