7a733b0508b8cc31416591d7d37416247cb601de
Georg Hinkel <georg.hinkel@hs-rm.de>
2025-03-10 14:02:24 +0100

    fix broken connector between components

--- Glsp/Glsp/Processing/EdgeContributionBase.cs
+++ Glsp/Glsp/Processing/EdgeContributionBase.cs
@@ -8,18 +8,21 @@ using System.Collections.Generic;
 namespace NMF.Glsp.Processing
 {
 
-    internal abstract class EdgeContributionBase<T> : ActionElement
+    internal abstract class EdgeContributionBase : ActionElement
+    {
+        public virtual void CreateEdge(GElement sourceElement, GElement targetElement, INotationElement parentNotation, CreateEdgeOperation createEdgeOperation, ISkeletonTrace trace)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    internal abstract class EdgeContributionBase<T> : EdgeContributionBase
     {
         public string ContributionId { get; } = Guid.NewGuid().ToString();
 
         public abstract void Contribute(T input, GElement element, ISkeletonTrace trace);
 
         public abstract IEnumerable<LabeledAction> CreateActions(GElement item, List<GElement> selected, string contextId, EditorContext editorContext);
-
-        public virtual void CreateEdge(GElement sourceElement, GElement targetElement, INotationElement parentNotation, CreateEdgeOperation createEdgeOperation, ISkeletonTrace trace)
-        {
-            throw new NotSupportedException();
-        }
 
         public abstract Type SourceType { get; }
 
