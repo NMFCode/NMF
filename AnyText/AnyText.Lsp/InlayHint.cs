dba42e394ae18d4c26f76159570fab7aa63f4422
robby-20 <weinert.kevin@gmx.de>
2025-03-21 09:58:27 +0100

    InlayHint classes implemented

---
+++ AnyText/AnyText.Lsp/InlayHint.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace NMF.AnyText
+{
+    [DataContract]
+    public partial class InlayHint
+    {
+
+        /// <summary>
+        /// The position of this hint.
+        /// </summary>
+        /// 
+        [DataMember(Name = "position")]
+        [JsonProperty(Required = Required.Always)]
+        public ParsePosition Position { get; init; }
+
+        /// <summary>
+        /// The label of this hint. A human readable string or an array of
+        /// InlayHintLabelPart label parts.
+        ///
+        /// *Note* that neither the string nor the label part can be empty.
+        /// </summary>
+        [DataMember(Name = "label")]
+        [JsonProperty(Required = Required.Always)]
+        public StringOrInlayHintLabelParts Label { get; init; }
+
+    }
+}
---
+++ AnyText/AnyText.Lsp/InlayHintParams.cs
@@ -0,0 +1,18 @@
+﻿using LspTypes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMF.AnyText
+{
+    public class InlayHintParams : TextDocumentPositionParams, IWorkDoneProgressParams
+    {
+        [DataMember(Name = "workDoneToken")]
+        [JsonProperty(Required = Required.Default, DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public SumType<string, int> WorkDoneToken { get; set; }
+    }
+}
