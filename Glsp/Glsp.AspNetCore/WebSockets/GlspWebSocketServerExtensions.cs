843ca453ff615a200b888b6c287fdd6ec7364262
Georg Hinkel <georg.hinkel@hs-rm.de>
2025-05-13 13:45:43 +0200

    supress StreamJsonRpc warnings

--- Glsp/Glsp.AspNetCore/WebSockets/GlspWebSocketServerExtensions.cs
+++ Glsp/Glsp.AspNetCore/WebSockets/GlspWebSocketServerExtensions.cs
@@ -50,7 +50,9 @@ namespace Microsoft.Extensions.DependencyInjection
                     using (var rpc = GlspJsonRpcServerUtil.CreateServer(socket, server))
                     {
                         rpc.StartListening();
+#pragma warning disable VSTHRD003 // Avoid awaiting foreign Tasks
                         await rpc.Completion;
+#pragma warning restore VSTHRD003 // Avoid awaiting foreign Tasks
                     }
 #if NET8_0_OR_GREATER
                     return Results.Empty;
