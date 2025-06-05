7d78dd7dc5917f0befedf3f83bfdb35fb8edbca7
Georg Hinkel <georg.hinkel@hs-rm.de>
2025-06-05 11:14:53 +0200

    add test for relative URIs

--- Models/Models.Tests/RelativeUriTests.cs
+++ Models/Models.Tests/RelativeUriTests.cs
@@ -31,5 +31,30 @@ namespace NMF.Models.Tests
             Assert.IsFalse(bContents.Contains(modelA.ModelUri.AbsoluteUri));
             Assert.IsTrue(bContents.Contains("a.x"));
         }
+
+        [TestMethod]
+        public void Repository_ResolvesLinkedModels()
+        {
+            var c1 = new Class { Name = "A" };
+            var c2 = new Class { Name = "B", BaseTypes = { c1 } };
+
+            var modelA = new Model { ModelUri = new Uri(Path.GetFullPath("a.x")), RootElements = { c1 } };
+            var modelB = new Model { ModelUri = new Uri(Path.GetFullPath("b.x")), RootElements = { c2 } };
+
+            var repo = new ModelRepository();
+            repo.Save(modelA, "a.x");
+            repo.Save(modelB, "b.x");
+
+            var repo2 = new ModelRepository();
+            var newModelB = repo2.Resolve("b.x");
+
+            Assert.IsTrue(repo2.Models.ContainsKey(modelA.ModelUri));
+            Assert.IsTrue(repo2.Models.ContainsKey(modelB.ModelUri));
+
+            var loadedC2 = (Class)newModelB.RootElements[0];
+            Assert.IsInstanceOfType(loadedC2, typeof(Class));
+            Assert.IsNotNull(loadedC2.BaseTypes.FirstOrDefault());
+            Assert.AreEqual(modelA.ModelUri, loadedC2.BaseTypes.FirstOrDefault().Model.ModelUri);
+        }
     }
 }
