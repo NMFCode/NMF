61a572ce1e6ed08993ab1413b22fef260c100ba3
Georg Hinkel <georg.hinkel@hs-rm.de>
2025-09-03 16:23:10 +0200

    prepare module histories for CI usage

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -3,6 +3,4 @@ using System.Resources;
 using System.Runtime.InteropServices;
 [assembly: AssemblyProduct(".NET Modelling Framework")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
--- Examples/Transformations.Sample/Properties/AssemblyInfo.cs
+++ Examples/Transformations.Sample/Properties/AssemblyInfo.cs
@@ -1,35 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("Transformations.Sample")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct(".Net Modeling Framework")]
-[assembly: AssemblyCopyright("Copyright © Georg Hinkel 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
-[assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("3f6236ec-cd63-4d73-9fd2-f11cdde41d62")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
